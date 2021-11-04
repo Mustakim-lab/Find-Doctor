5d8fb982e107094897c1c1fda6a2ad4550a880c9
HP <mustakim15-2563@diu.edu.bd>
2021-11-04 19:38:50 +0600

    design serial activity and set listener doctor and serial

--- app/src/main/java/com/example/finddoctor/MainActivity.java
+++ app/src/main/java/com/example/finddoctor/MainActivity.java
@@ -3,30 +3,49 @@ package com.example.finddoctor;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle toggle;
     NavigationView navigationView;
+
+    private CardView doctorCard,serialCard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        doctorCard =findViewById(R.id.doctorCard_ID);
+        doctorCard.setOnClickListener(this);
+        serialCard=findViewById(R.id.serialCard_ID);
+        serialCard.setOnClickListener(this);
+
         drawerLayout=findViewById(R.id.drawer_ID);
         navigationView=findViewById(R.id.navigation_ID);
-
         toggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
     }
 
     @Override
@@ -37,11 +56,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId()==R.id.home_Id){
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId()==R.id.doctorCard_ID){
+            Intent intent=new Intent(MainActivity.this,doctorList.class);
+            startActivity(intent);
+        }else if (v.getId()==R.id.serialCard_ID){
+            Intent intent=new Intent(MainActivity.this,serilaIntroduction.class);
+            startActivity(intent);
         }
-        return false;
     }
 }
