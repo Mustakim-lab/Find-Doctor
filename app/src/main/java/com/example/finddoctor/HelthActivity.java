b78893c4744630f26a839a3178dfcc892b9b4618
HP <mustakim15-2563@diu.edu.bd>
2021-11-10 12:54:25 +0600

    complete helth card

--- app/src/main/java/com/example/finddoctor/HelthActivity.java
+++ app/src/main/java/com/example/finddoctor/HelthActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.example.finddoctor.Adapter.HelthAdapter;
 
@@ -11,19 +12,20 @@ public class HelthActivity extends AppCompatActivity {
 
     private ListView listView;
     String[] name;
-    String[] child;
-    int[] icon={R.drawable.protin};
+    int[] icon={R.drawable.protin,R.drawable.fat,R.drawable.karbohaitet,R.drawable.vitamin};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_helth);
 
         name=getResources().getStringArray(R.array.khabarer_name);
-        child=getResources().getStringArray((R.array.biboron));
+
 
         listView=findViewById(R.id.listView_ID);
 
+
         HelthAdapter helthAdapter=new HelthAdapter(HelthActivity.this,name,icon);
         listView.setAdapter(helthAdapter);
+
     }
 }
